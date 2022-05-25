8221e76ce02ef3f8ea8c1f2781fbd23ef8ba2072
Minggao <chaosflamee@gmail.com>
2022-05-25 14:20:58 +0200

    Replace the code by Noa

--- js/script.js
+++ js/script.js
@@ -6,22 +6,23 @@ function ajouter(){
     console.log(document.forms.newTaskF.tache.value);
     console.log(document.forms.newTaskF.date.value);
 
-    //création des variable pour la création d'une nouvelle ligne dans le tableau
-    const newItem = document.createElement('tr')
-    const taskTd = document.createElement('td')
-    const dateTd = document.createElement('td')
-    const categorieTd = document.createElement('td')
-    taskTd.textContent = document.newTaskF.tache.value
-    dateTd.textContent = document.newTaskF.date.value
-    categorieTd.textContent = document.newTaskF.categorie.value
-    
-    //const selectEntree = document.getElementById("entreeId");
-    //const valeurselectionnee = selectEntree.options[selectEntree.selectedIndex].value;
-    //const textselectionne = selectEntree.options[selectEntree.selectedIndex].text;
-   //Vérification de la récupération
-    console.log(taskTd.textContent)
-    console.log(dateTd.textContent)
-    console.log(categorieTd.textContent)
+    // permet de récupérer les tâches entrées puis les ajoute dans myTasks
+    supprimer();
+    pushTache(tacheSaisie());
+
+    myTasks.forEach((tasks)=>{
+      //création des variable pour la création d'une nouvelle ligne dans le tableau
+      const newItem = document.createElement('tr')
+      const taskTd = document.createElement('td')
+      const dateTd = document.createElement('td')
+      const categorieTd = document.createElement('td')
+      taskTd.textContent = tasks.name
+      dateTd.textContent = tasks.date
+      categorieTd.textContent = tasks.category
+      //Vérification de la récupération
+      console.log(taskTd.textContent)
+      console.log(dateTd.textContent)
+      console.log(categorieTd.textContent)
 
     if (!document.newTaskF.tache.checkValidity() ||
           !document.newTaskF.date.checkValidity() ||
@@ -29,18 +30,17 @@ function ajouter(){
          ) {
                return
         }
-
-    if(taskTd.textContent=="BOT_RUN"){
-        activate_bot();
-    }
-    
-    //const table = document.querySelector('table')
+        
     newItem.append(taskTd, dateTd, categorieTd)
 
      /* le premier élément dans le document qui contient la classe "datatable" est retourné*/
     const table = document.querySelector('.datatable tbody')
-    /*  Ex2)3)vi) */
-    table.appendChild(newItem)
+    table.appendChild(newItem);
+    })
+
+   
+    
+    
 
 }
 
@@ -51,4 +51,53 @@ function supprimer() {
           tbody.removeChild(tbody.firstChild)
         }
     
+}
+
+function supprimer_tout() {
+  myTasks = []
+  const tbody = document.querySelector('.datatable tbody' )
+  while (tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild)
+  }
+
+}
+
+function tacheSaisie(){
+
+  name_task = document.newTaskF.tache.value;
+  date_task = document.newTaskF.date.value;
+  category_task = document.newTaskF.categorie.value;
+  
+  /*
+  //question 2
+  let myNewTask = {
+
+    name: name_task,
+    date: date_task,
+    category: category_task,
+  
+  }
+  return myNewTask;
+  */
+
+  
+  const newTache = new Tache(name_task, date_task, category_task); 
+  return newTache;
+  
+}
+
+class Tache{
+
+  constructor(name, date, category){
+
+    this.name = name;
+    this.date = date;
+    this.category = category;
+  }
+}
+
+var myTasks = new Array;
+
+function pushTache(Tache){
+  myTasks.push(Tache);
 }
